16d4e5fcce5ff345d32588c2a4d5f0b0aeebecfa
baryska <barbora.skalova@newtontech.cz>
2022-07-19 23:20:13 +0200

    change intro page + some styling

--- components/Statements/Statements.tsx
+++ components/Statements/Statements.tsx
@@ -7,27 +7,19 @@ function Statements() {
   return (
     <div className={styles.statements} >
       <div>
-        <p className={styles.statement}>Věděli jste, že v příštích 5 letech přibyde v Berouně kvůli neregulované developerské výstavbě 6 500 nových obyvatel?</p>
+        <p className={styles.statement}>Věděli jste, že <strong>v příštích 5 letech</strong> přibyde v Berouně kvůli neregulované developerské výstavbě <strong>1 800 bytů a 6 500 nových obyvatel</strong>?</p>
       </div>
       <BlueDots />
       <div>
-        <p className={styles.statement}><strong>Neprodáme</strong> Beroun developerům<span>.</span></p>
+        <p className={styles.statement}>Věděli jste, že Beroun nemá na <strong>nárůst obyvatel o 30 %</strong> vůbec připravenou infrastrukturu - <strong>chybí školy, školky, obchvat města i voda</strong>?</p>
       </div>
       <BlueDots />
       <div>
-        <p className={styles.statement}>Konečně zajistíme <strong>dostatek míst ve školách a školkách</strong><span>.</span></p>
+        <p className={styles.statement}>My Beroun developerům <strong>neprodáme</strong>.</p>
       </div>
       <BlueDots />
       <div>
-        <p className={styles.statement}><strong>Zklidníme</strong> dopravu<span>.</span></p>
-      </div>
-      <BlueDots />
-      <div>
-        <p className={styles.statement}>Zajistíme <strong>alternativní zdroj vody</strong><span>.</span></p>
-      </div>
-      <BlueDots />
-      <div>
-        <p className={styles.statement}>Budeme s vámi mluvit - <strong>bez arogance</strong><span>.</span></p>
+        <p className={styles.statement}>A budeme s vámi mluvit - <strong>bez arogance</strong>.</p>
       </div>
      
       <div className={styles.buttonDiv} >
